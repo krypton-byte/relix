3d54cf1aa3af9c6c57ae40bd1b256b59d2374ade
krypton-byte <rosid6434@gmail.com>
2023-04-24 11:19:44 +0700

    cov: test

--- relix/tests/test_i.py
+++ relix/tests/test_i.py
@@ -3,8 +3,7 @@ import unittest
 
 class RelixTest(unittest.TestCase):
     def test_import_modules(self):
-        from .load import COMMAND
-        self.assertTrue(bool(COMMAND))
+        self.assertTrue(bool(__import__('relix.command').command))
 
 
 if __name__ == '__main__':
